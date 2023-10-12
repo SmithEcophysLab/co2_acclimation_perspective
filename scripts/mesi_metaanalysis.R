c0dde5a61db498069ce67450efdcb24a878e8b94
eaperkowski <evan.a.perkowski@ttu.edu>
2023-10-12 10:26:48 -0500

    add script for MESI meta-analysis. also contains figure code

--- scripts/mesi_metaanalysis.R
+++ scripts/mesi_metaanalysis.R
@@ -5,30 +5,30 @@ library(tidyverse)
 library(metafor)
 library(MAd)
 
-## Load MESI dataset
+## Load MESI dataset (repository cloned from 
+## https://github.com/MESI-organization/mesi-db)
 df <- read.csv("../../mesi-db/data/mesi_main.csv") 
 
 ## See trait options
 unique(df$response)
 vars_of_interest <- c("anet", "jmax", "leaf_n_mass", 
                       "root_shoot_ratio", "total_biomass", "vcmax",
-                      "soil_no3-n", "soil_nh4-n", "soil_in",
+                      "soil_no3-n", "soil_nh4-n", "soil_in", "gs",
                       "root_n_uptake", "root_nh4_uptake", "root_no3_uptake",
                       "wue", "lai", "lai_max", "gpp", "asat")
-
 
 ## Assess treatment combinations. Searching for CO2 x N experiments
 unique(df$treatment)
 
-## Filter MESI to include CO2 fertilization experiments with defined
-## experiment medium and only traits of interest
+## Filter MESI to include CO2 fertilization experiments with only traits 
+## of interest
 df_filtered <- df %>%
   
-  # Filter to include CO2 x fertilization and CO2-only experiments
+  # Filter to include CO2-only experiments
   filter(treatment == "c") %>%
   
   # Remove experiments without defined medium
-  filter(!is.na(experiment_type)) %>%
+  #filter(!is.na(experiment_type)) %>%
   
   # Only include relevant measurements
   filter(response %in% vars_of_interest) %>%
@@ -70,60 +70,79 @@ log_resp_agg <- log_resp %>%
          response = ifelse(response %in% c("root_n_uptake", "root_nh4_uptake", 
                                            "root_no3_uptake"),
                            "n_uptake", response),
-         response = ifelse(response %in% c("lai", "lai_max"), "lai", response))
+         response = ifelse(response %in% c("lai", "lai_max"), "lai", response)) %>%
+  filter(logr > -2)
 
-
-vars_of_interest2 <- c("anet", "jmax", "leaf_n_mass", 
+## Create new vars of interest vector with new "available_n", "n_uptake", and
+## "lai" cols
+vars_of_interest2 <- c("anet", "jmax", "leaf_n_mass", "gs",
                        "root_shoot_ratio", "total_biomass", "vcmax",
                        "available_n", "n_uptake", "lai", "gpp", "asat")
 
+## Pull analyse_meta.R script from Beni's repository to replicate approach used
+## in Nwg meta-analysis
 source("../../lt_cn_review/R/analyse_meta.R")
 out  <- purrr::map(as.list(vars_of_interest2), 
-                   ~analyse_meta(log_resp_agg %>% rename(var = response), 
-                                                    nam_target = .))
+                   ~analyse_meta(log_resp_agg %>% 
+                                   rename(var = response),
+                                 nam_target = .))
 names(out) <- vars_of_interest2
 df_box <- purrr::map_dfr(out, "df_box") %>%
   mutate(var = factor(var, 
                       levels = c("available_n", "n_uptake", "root_shoot_ratio",
                                  "total_biomass", "lai", "gpp",
-                                 "wue", "jmax", "vcmax", "asat", 
-                                 "anet", "leaf_n_mass")))
+                                 "wue", "gs", "leaf_n_mass", "jmax", "vcmax", "asat", 
+                                 "anet")))
 
-## Make plot
+## Subset only needed vars, factor to desirable plot order that 
+## corresponds with leaf-whole plant-ecosystem levels
 log_resp_agg_plot_subset <- log_resp_agg %>%
   filter(response %in% c("anet", "jmax", "leaf_n_mass", 
-         "root_shoot_ratio", "total_biomass", "vcmax",
+         "root_shoot_ratio", "total_biomass", "vcmax", "gs",
          "available_n", "lai", "gpp", "asat", "n_uptake"))
-
 
 log_resp_agg_plot_subset$response <- factor(log_resp_agg_plot_subset$response,
                                 levels = c("available_n", "n_uptake", 
                                            "root_shoot_ratio",
                                            "total_biomass", "lai", "gpp",
-                                           "wue", "jmax", "vcmax", "asat", 
-                                           "anet", "leaf_n_mass"))
-                                  
+                                           "wue", "gs", "leaf_n_mass", "jmax", 
+                                           "vcmax", "asat", 
+                                           "anet"))
+
+# Make plot
+#png("../figs/co2_perspective_general_trends.png", height = 4.5, width = 8, 
+#    units = "in", res = 600)                               
 ggplot(data = log_resp_agg_plot_subset, aes(x = response, y = logr)) +
-  geom_jitter(alpha = 0.5, width = 0.25) +
-  geom_crossbar(data = df_box, 
-                aes(x = var, y = middle, ymin = ymin, ymax = ymax),
-                fill = "turquoise", color = "black", alpha = 0.6, width = 0.5) +
+  geom_jitter(aes(size = logr_se), alpha = 0.4, width = 0.20, shape = 21) +
+  geom_errorbar(data = df_box, 
+                aes(x = var, y = middle, ymin = ymin, ymax = ymax), 
+                width = 0.5, size = 1) +
+  geom_point(data = df_box, aes(x = var, y = middle), 
+             size = 2, shape = 21, fill = "black") +
+  # geom_crossbar(data = df_box, 
+  #               aes(x = var, y = middle, ymin = ymin, ymax = ymax),
+  #               fill = "turquoise", color = "black", alpha = 0.6, width = 0.5) +
   scale_x_discrete(labels = c("Inorganic N availability", 
                               "Root N uptake",
                               "Root:shoot", 
                               "Total biomass", 
                               "LAI", 
-                              "GPP", 
+                              "GPP",
+                              expression("g"["s"]),
+                              expression("N"["mass"]),
                               expression("J"["max"]), 
                               expression("V"["cmax"]),
                               expression("A"["sat"]),
-                              expression("A"["net"]),
-                              expression("N"["mass"]))) +
-  geom_hline(yintercept = 0, size = 1, linetype = "dashed") +
-  labs(x = "Response variable", y = expression("Response to elevated CO"["2"])) + 
+                              expression("A"["net"]))) +
+  geom_hline(yintercept = 0, size = 1, linetype = "dashed", color = "red") +
+  labs(x = "Response variable", y = expression("Response to elevated CO"["2"]),
+       size = "Standard error of\nlog response ratio") + 
+  scale_y_continuous(limits = c(-0.8, 1.2), 
+                     breaks = c(-0.8, -0.4, 0.0, 0.4, 0.8, 1.2)) +
   coord_flip() +
-  theme_bw()
-
+  theme_bw() +
+  theme(panel.grid.major.y = element_blank())
+#dev.off()
 
 
 
